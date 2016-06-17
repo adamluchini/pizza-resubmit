f4d10b78896cd0fb52b450323015377d1ae03889
Adam Luchini and Adam Sander Luchini <adam@>
2016-06-17 15:30:31 -0700

    prior to pizza multiplyer

---
+++ README.md
@@ -0,0 +1,30 @@
+# _Pizza Ordering Website
+
+#### _{A website that turns an input number into a fun list of numbers with text substitutions}, {June 17th, 2016}_
+
+#### By _**{Adam Luchini}**_
+
+## Description
+
+_{The Pizza Ordering Website generates the price of a pizza based on the following parameters:
+  "Size of pizza (small, medium, large)""
+  "Number of desired pizzas""
+  "Number of toppings, from a list of 4."}_
+
+## Technologies Used
+
+_{HTML, CSS, Bootstrap, JavaScript, jquery 3.0.0,}_
+
+### License
+
+*{This program is licensed under MIT}*
+
+### Specifications
+*Generates a price based on the size of pizza the user selects.
+-Example: if the user inputs medium, it will generate a base price of $13.
+Generates a price based on the number of toppings the user selects at a $1 each.
+-Example: if the user selects pepperoni and sausage, it will add $2 to the total price.
+Multiplies the total price based on the number of pizzas the user chooses.
+-Example: 2 medium 2 topping pizzas would be $30.
+
+Copyright (c) 2016 **_{Adam Luchini}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
+//business class
+
 function Topping (pizzaTopping1, pizzaTopping2, pizzaTopping3, pizzaTopping4){
   this.pizzaTopping1=pizzaTopping1;
   this.pizzaTopping2=pizzaTopping2;
   this.pizzaTopping3=pizzaTopping3;
   this.pizzaTopping4=pizzaTopping4;
 }
-
 
 function PizzaOrder (quantity, pizzaSize){
   this.quantity=quantity;
@@ -21,7 +22,6 @@ Topping.prototype.additionalCost=function(){
     additionalCost +=1;
   } if (this.pizzaTopping4==="sausage"){
     additionalCost +=1;
-  } else{
 
   }
   return additionalCost
@@ -30,57 +30,17 @@ Topping.prototype.additionalCost=function(){
 PizzaOrder.prototype.sizePrice=function(){
   var orderCost=0;
   if(this.pizzaSize === "small"){
-    orderCost += 12;
+    orderCost += 10;
   } else if(this.pizzaSize==="medium"){
-    orderCost += 15;
+    orderCost += 13;
   } else {
-    orderCost +=20;
+    orderCost +=15;
   }
 
 var orderPrice=orderCost*this.quantity;
 return orderPrice
 }
-//
-// PizzaOrder.prototype.additionalCost=function(){
-//   var additionalCost=0;
-//   if(this.pizzaTopping1=== "pepperoni"){
-//   additionalCost+=10;
-//   } else {
-//   additionalCost+=0;
-//   }
-//   return additionalCost
-// }
-//
-// PizzaOrder.prototype.additionalCost2=function(){
-//   var additionalCost2=0;
-//   if(this.pizzaTopping2=== "olives"){
-//   additionalCost2+=10;
-//   } else {
-//   additionalCost2+=0;
-//   }
-//   return additionalCost2
-// }
-//
-// PizzaOrder.prototype.additionalCost3=function(){
-//   var additionalCost3=0;
-//   if(this.pizzaTopping3=== "peppers"){
-//   additionalCost3+=10;
-//   } else {
-//   additionalCost3+=0;
-//   }
-//   return additionalCost3
-// }
-//
-// PizzaOrder.prototype.additionalCost4=function(){
-//   var additionalCost4=0;
-//   if(this.pizzaTopping4=== "sausage"){
-//   additionalCost4+=10;
-//   } else {
-//   additionalCost4+=0;
-//   }
-//   return additionalCost4
-// }
-
+//user interface
 $(document).ready(function(){
   $("form#order").submit(function(event){
     event.preventDefault();
@@ -95,15 +55,9 @@ $(document).ready(function(){
     var newPizzaOrder=new PizzaOrder(inputtedQuantity, inputtedSize);
     var newToppingList=new Topping(pizzaTopping1, pizzaTopping2, pizzaTopping3, pizzaTopping4);
 
-    console.log(pizzaTopping1);
-    console.log(pizzaTopping2);
-    // console.log(inputtedTopping3);
-    // console.log(inputtedTopping4);
     var price=newPizzaOrder.sizePrice();
     var additionalCost=newToppingList.additionalCost();
-    // var additionalCost2=newPizzaOrder.additionalCost2();
-    // var additionalCost3=newPizzaOrder.additionalCost3();
-    // var additionalCost4=newPizzaOrder.additionalCost4();
+
     var grandTotal=price+additionalCost;
     $("#result").show(function(){
       $("#order-price").text(grandTotal);
